3ee92e0a1efa48317791984443212f1b24ee7297
Martin Toman <toman.martin@live.com>
2020-10-10 12:58:21 +0200

    Switch to niv+lorri

--- shell.nix
+++ shell.nix
@@ -1,30 +1,17 @@
-let
-  # Look here for information about how to generate `nixpkgs-version.json`.
-  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-  pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
-  pinnedPkgs = import (builtins.fetchGit {
-    inherit (pinnedVersion) url rev;
+{ sources ? import ./nix/sources.nix }:
 
-    ref = "nixos-unstable";
-  }) {};
-in
+with { overlay = _: pkgs: {
+  niv          = import sources.niv { };
+}; };
 
-# This allows overriding pkgs by passing `--arg pkgs ...`
-{ pkgs ? pinnedPkgs }:
-
-with pkgs;
+with import sources.nixpkgs {
+  overlays = [ overlay ];
+  config = { };
+};
 
 mkShell {
   buildInputs = [
     go
     gotools
   ];
-  shellHook = ''
-    [[ -d .gocache ]] || mkdir .gocache
-    [[ -d .goroot ]]  || mkdir .goroot
-    export GOCACHE=$(pwd)/.gocache
-    export GOPATH=$(pwd)/.goroot
-
-    [ -e .env ] && source .env
-  '';
 }
